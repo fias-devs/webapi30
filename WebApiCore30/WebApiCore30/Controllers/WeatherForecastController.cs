debaf87d032cbcbdc9fa8cfcba6682b99995e8e7
fias-devs <55956590+fias-devs@users.noreply.github.com>
2019-12-15 20:09:54 +0100

    Update WeatherForecastController.cs

--- WebApiCore30/WebApiCore30/Controllers/WeatherForecastController.cs
+++ WebApiCore30/WebApiCore30/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebApiCore30.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Make kralj"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
