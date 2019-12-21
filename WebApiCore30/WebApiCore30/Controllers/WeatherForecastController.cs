9636c43ce1c9bc12a67c3284294e6ee94a8e000e
fias-devs <55956590+fias-devs@users.noreply.github.com>
2019-12-21 13:41:33 +0100

    Update WeatherForecastController.cs

--- WebApiCore30/WebApiCore30/Controllers/WeatherForecastController.cs
+++ WebApiCore30/WebApiCore30/Controllers/WeatherForecastController.cs
@@ -26,14 +26,7 @@ namespace WebApiCore30.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return new List<string>(){$"Mahe car{DateTime.Now}"};
         }
     }
 }
