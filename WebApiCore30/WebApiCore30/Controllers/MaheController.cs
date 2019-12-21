8e2e331cde2020c25c307e36dcb856d55cbcf926
Denis Music <denis@edu.fit.ba>
2019-12-21 19:52:36 +0100

    Mahe cotroller added

---
+++ WebApiCore30/WebApiCore30/Controllers/MaheController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiCore30.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MaheController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return $"Pravi Mahe kontroler -> {DateTime.Now} ";
+        }
+    }
+}
