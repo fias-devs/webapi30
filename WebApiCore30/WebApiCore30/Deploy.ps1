b118336a90876261f4693805ed2526d3aa113f99
Denis Music <denis@edu.fit.ba>
2020-01-06 22:44:07 +0100

    ...

--- WebApiCore30/WebApiCore30/Deploy.ps1
+++ WebApiCore30/WebApiCore30/Deploy.ps1
@@ -1,3 +1,3 @@
 $connectionString = $OctopusParameters["webapicore.dev.db.connectionstring"]
-
+ls
 .\migrate.exe webapicore30.dll /connectionString="$($connectionString)" /connectionProviderName="System.Data.SqlClient"
