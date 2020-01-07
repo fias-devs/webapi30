788183256dfb54dde9c01ed17ba8113eeba5f858
denis <denis@edu.fit.ba>
2020-01-07 11:02:04 +0100

    ....

--- WebApiCore30/WebApiCore30/Controllers/WeatherForecastController.cs
+++ WebApiCore30/WebApiCore30/Controllers/WeatherForecastController.cs
@@ -27,7 +27,7 @@ namespace WebApiCore30.Controllers
         [HttpGet]
         public string Get()
         {
-            return $"FIT Hello from nastava @ {DateTime.Now} &** Haris car since {DateTime.Now} ";
+            return $"Trenutno vrijeme na serveru -> {DateTime.Now} ";
         }
     }
 }
